7e28a27efedb2d3577bf5eb7f68db907c3e8985e
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-26 10:08:53 +0100

    Add test for editTask and localStorage

--- src/__tests__/crud.test.js
+++ src/__tests__/crud.test.js
@@ -1,6 +1,6 @@
-import MockStorage from '../../__mock__/storage';
-import check from '../modules/checkComplete';
-import deleteCompleted from '../modules/deleteCompleted';
+import MockStorage from '../../__mock__/storage.js';
+import check from '../modules/checkComplete.js';
+import deleteCompleted from '../modules/deleteCompleted.js';
 import editTask from '../modules/editTask.js';
 
 describe('Check Function', () => {
@@ -18,7 +18,6 @@ describe('Check Function', () => {
     expect(item.completed).toBe(false);
   });
 });
-
 
 describe('Local Storage test on check function', () => {
   test('Update the local storage status from false to true', () => {
@@ -39,13 +38,13 @@ describe('Local Storage test on check function', () => {
     const todos = [];
     const newTaskOne = { description: 'task 1', completed: false, index: 1 };
     todos.push(newTaskOne);
-    const newTaskTwo =  { description: 'task 2', completed: true, index: 2 };
-    todos.push(newTaskTwo)
+    const newTaskTwo = { description: 'task 2', completed: true, index: 2 };
+    todos.push(newTaskTwo);
     mockStorage.setItem('to-do-list', todos);
     const arr = mockStorage.getItem('to-do-list');
     check(box, arr[1]);
     expect(arr[1].completed).toBe(false);
-    });
+  });
 });
 
 describe('DeleteCompleted Function', () => {
@@ -62,23 +61,36 @@ describe('DeleteCompleted Function with LocalStorage', () => {
     const todos = [];
     const newTaskOne = { description: 'task 1', completed: true, index: 1 };
     todos.push(newTaskOne);
-    const newTaskTwo =  { description: 'task 2', completed: false, index: 2 };
-    todos.push(newTaskTwo)
+    const newTaskTwo = { description: 'task 2', completed: false, index: 2 };
+    todos.push(newTaskTwo);
     mockStorage.setItem('to-do-list', todos);
     const arr = mockStorage.getItem('to-do-list');
     const uncompleted = deleteCompleted(arr);
     mockStorage.setItem('to-do-list', uncompleted);
     const getUncompleted = mockStorage.getItem('to-do-list');
     expect(getUncompleted.length).toBe(1);
-    
   });
 });
 
 describe('EditTask Function', () => {
   test('EditTask function edit the description of input', () => {
-    const todos =[{index:1, description: 'TestContext'},{index:2, description:'TestOne'}];
-    const updateText = {textContent: 'UpdateTest'};
-    const updateTask = editTask(updateText,todos,todos[0]);
+    const todos = [{ index: 1, description: 'TestContext' }, { index: 2, description: 'TestOne' }];
+    const updateText = { textContent: 'UpdateTest' };
+    const updateTask = editTask(updateText, todos, todos[0]);
     expect(updateTask[0].description).toBe('UpdateTest');
+  });
+});
+
+describe('Edit task Function with LocalStorage', () => {
+  test('updates item in localStorage', () => {
+    const mockStorage = new MockStorage();
+    const todos = [{ description: 'First task', index: 1 }, { description: 'Second Task', index: 2 }];
+    mockStorage.setItem('to-do-list', todos);
+    const getTasks = mockStorage.getItem('to-do-list');
+    const someText = { textContent: 'First task - Changed' };
+    const updatedTasks = editTask(someText, getTasks, getTasks[0]);
+    mockStorage.setItem('to-do-list', updatedTasks);
+    const getUpdatedTasks = mockStorage.getItem('to-do-list');
+    expect(getUpdatedTasks[0].description).toBe('First task - Changed');
   });
 });
--- src/modules/editTask.js
+++ src/modules/editTask.js
@@ -2,8 +2,7 @@ const editTask = (description, items, item) => {
   const itemIndex = item.index;
   const itemToEdit = items[itemIndex - 1];
   itemToEdit.description = description.textContent;
-
-  return items
+  return items;
 };
 
 export default editTask;
